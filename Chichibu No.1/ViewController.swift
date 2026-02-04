2d40c2c1647dabd4400d30a7183788d58877b6a7
kappaMiwa <umeumebosi2000@yahoo.co.jp>
2026-02-04 18:36:02 +0900

    追加しました。動くかどうか一度確認をお願いします。

--- Chichibu No.1/ViewController.swift
+++ Chichibu No.1/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Chichibu No.1
 //
-//  Created by Ayane Ando gakuto on 11/20/25.
+//  Created by Tahara Yuko on 2026/02/04.
 //
 
 import UIKit
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
