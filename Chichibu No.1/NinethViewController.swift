2cf5423453e1cac680ccda8909fd85a9308327c2
kappaMiwa <umeumebosi2000@yahoo.co.jp>
2026-02-06 17:57:07 +0900

    もう一度やり直してみました。

---
+++ Chichibu No.1/NinethViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NinethViewController.swift
+//  Chichibu No.1
+//
+//  Created by Tahara Yuko on 2026/02/06.
+//
+
+import UIKit
+
+class NinethViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
