20d26ca9ea8706be5deb416d4ebc95ef7d128730
KeiOno-03 <etsuonotkc@gmal.com>
2026-02-28 06:02:51 +0900

    少し進めました。

---
+++ Chichibu No.1/nineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  nineViewController.swift
+//  Chichibu No.1
+//
+//  Created by 小野悦子 on 2026/02/28.
+//
+
+import UIKit
+
+class nineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
