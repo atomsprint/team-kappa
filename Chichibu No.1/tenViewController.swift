a0492c5aab63a65b860542c21aa532e007377257
ayane302 <ayane.ando.20150302@gmail.com>
2026-04-25 12:28:22 +0900

    みわのコードちょっと書いておきました。　あとは岳斗に説明の画面を作って貰えばOKです。

--- Chichibu No.1/tenViewController.swift
+++ Chichibu No.1/tenViewController.swift
@@ -11,6 +11,7 @@ class tenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
