08a1a4cdb9f54df6b96db6d6a70e8790c8771cb5
ayane302 <ayane.ando.20150302@gmail.com>
2026-05-02 07:28:27 +0900

    改行入れました

--- Chichibu No.1/nature_sub_ViewController.swift
+++ Chichibu No.1/nature_sub_ViewController.swift
@@ -13,8 +13,8 @@ class nature_sub_ViewController: UIViewController {
     @IBOutlet weak var nature_explanation: UILabel!
     @IBOutlet weak var nature_image: UIImageView!
     let al_nature_image = [#imageLiteral(resourceName: "秩父雲海"), #imageLiteral(resourceName: "宝登山"), #imageLiteral(resourceName: "羊山公園"), #imageLiteral(resourceName: "武甲山"), #imageLiteral(resourceName: "栗谷瀬橋の蛇紋岩"), #imageLiteral(resourceName: "橋立鍾乳洞")]
-    let al_nature_name = ["秩父雲海", "宝登山", "洋山公園", "武甲山", "栗谷瀬橋の蛇紋岩", "橋立鍾乳洞"]
-    let al_nature_explanation = ["秩父ミューズパークにある。春と秋の明け方に雲海が発生する。前日に雨が降った日は特に発生しやすい。ミューズパークからは、ハーブ橋がかかっている。", "蝋梅が咲く２月ごろがおすすめ", "４月下旬に咲く４０万株の芝桜は圧巻。朝一番で見ることができるので、５時過ぎがベスト", "ヤマトタケルが登山し、武具を納め統制の成功を祈った伝説がある。山頂からはお祭りの打ち上げ花火を見下ろすことができる。", "長瀞の岩棚に代表される三波川帯の中に小規模な蛇紋岩帯が分布している。蛇紋岩は磨くと美しい石材となり、緑色の大理石とも呼ばれる。", "竪穴型の鍾乳洞で、宮沢賢治も訪れた場所。"]
+    let al_nature_name = ["秩父雲海", "宝登山", "羊山公園", "武甲山", "栗谷瀬橋の蛇紋岩", "橋立鍾乳洞"]
+    let al_nature_explanation = ["秩父ミューズパークにある。\n春と秋の明け方に雲海が発生する。\n前日に雨が降った日は特に発生しやすい。\nミューズパークからは、ハーブ橋がかかっている。", "蝋梅が咲く２月ごろがおすすめ", "４月下旬に咲く４０万株の芝桜は圧巻。朝一番で見ることができるので、\n５時過ぎがベスト", "ヤマトタケルが登山し、武具を納め統制の成功を祈った伝説がある。\n山頂からはお祭りの打ち上げ花火を見下ろすことができる。", "長瀞の岩棚に代表される三波川帯の中に小規模な蛇紋岩帯が分布している。\n蛇紋岩は磨くと美しい石材となり、\n緑色の大理石とも呼ばれる。", "竪穴型の鍾乳洞で、\n宮沢賢治も訪れた場所。"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
